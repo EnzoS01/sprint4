5dc7bf9757f76c41999e6d153221c59a14bd9274
EnzoS01 <cocenzosilva@gmail.com>
2023-10-18 19:47:11 -0300

    firs commit

---
+++ src/main/java/com/example/Sprint4/PrimerApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.Sprint4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerApiApplication.class, args);
+		System.out.println("Estoy Funcionando");
+	}
+
+}
---
+++ src/main/java/com/example/Sprint4/repositories/ArticuloManufacturadoRepository.java
@@ -0,0 +1,6 @@
+package com.example.Sprint4.repositories;
+
+import com.example.Sprint4.entities.ArticuloManufacturado;
+
+public interface ArticuloManufacturadoRepository extends BaseRepository<ArticuloManufacturado, Long>{
+}
